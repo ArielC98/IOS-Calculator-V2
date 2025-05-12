fa9c34245dfd17d14da5f37f7abd1f7dedf7d958
ArielC98 <arielcalderon100@gmail.com>
2025-05-11 23:08:48 -0500

    Initial structure

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {
+  StatusBar,
+  Text,
+  View,
+} from 'react-native';
+import { CalculatorScreen } from './screens/CalculatorScreen';
+
+
+
+function App() {
+
+  return (
+    <View>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={'black'}
+      />
+      <CalculatorScreen/>
+    </View>
+  );
+}
+
+
+
+export default App;
